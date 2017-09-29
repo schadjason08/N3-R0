f09a048b0d224f89b3615e0435a63b54d2885ce1
schadjason08 <schadjason08@gmail.com>
2017-09-29 03:08:30 -0500

    playerMovement Script Created

---
+++ N3-R0/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    private RigidBody2D myRigidbody;
+
+    [SerializedField]
+    private float movementSpeed;
+
+    private bool faceingRight;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        faceingRight = true;
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+
+        HandleMovement();
+
+        Flip(horizontal);
+	}
+
+    private void HandleMovement(float horizontal)
+    {
+
+
+        myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+
+
+
+    }
+
+    private void Flip(float horizontal)
+    {
+
+        if (horizontal > 0 && !facingRight || horizontal < 0 && faceingRight)
+        {
+            faceingRight = !faceingRight;
+
+            Vector3 theScale = transform.localScale;
+
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+        }
+
+    }
+}
